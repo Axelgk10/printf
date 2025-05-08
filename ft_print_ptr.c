a6ad71fd0f7384b946db6837769a46735bea5835
Axelgk10 <axelkonert@gmail.com>
2025-05-08 16:53:31 +0200

    Update ft_print_ptr.c

--- ft_print_ptr.c
+++ ft_print_ptr.c
@@ -1,8 +1,7 @@
-#include "printf.h"
 #include <stdio.h>
-#include <unistd.h>
+#include <unistd.h>  // Necesario para la función write()
 
-int ft_print_ptr(void   *ptr)
+int ft_print_ptr(void *ptr)
 {
     unsigned long   address;
     int             count;
@@ -10,11 +9,11 @@ int ft_print_ptr(void   *ptr)
     count = 0;
     if (!ptr)
     {
-        count += write (1, "nil", 3);
+        count += write(1, "nil", 3);
         return (count);
     }
     address = (unsigned long)ptr;
-    count += write (1, "0x", 2);
+    count += write(1, "0x", 2);
     count += ft_print_hex(address, 0);
     return (count);
 }
@@ -33,7 +32,7 @@ int ft_print_hex(unsigned long num, int uppercase)
     else
         digits = "0123456789abcdef";
     if (num == 0)
-        write (1, "0", 1);
+        write(1, "0", 1);
     while (num > 0)
     {
         buffer[i++] = digits[num % 16];
@@ -41,10 +40,14 @@ int ft_print_hex(unsigned long num, int uppercase)
     }
     while (i > 0)
     {
-        count += write (1, &buffer[--i], 1);
+        count += write(1, &buffer[--i], 1);
     }
     return (count);
 }
+
+int ft_print_ptr(void *ptr);
+int ft_print_hex(unsigned long num, int uppercase);
+
 int main(void)
 {
     int num = 42;
@@ -79,9 +82,6 @@ int main(void)
     printf("ft_print_ptr: ");
     count = ft_print_ptr(&ft_print_ptr);
     printf("\nCaracteres impresos: %d\n", count);
-
-    return 0;
-}
 
     return 0;
 }
