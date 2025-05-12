81422a719155e59ba00b430bd20ebcc4d253a89f
Axelgk10 <axelkonert@gmail.com>
2025-05-12 11:53:45 +0200

    Update ft_print_hex_lower.c

--- ft_print_hex_lower.c
+++ ft_print_hex_lower.c
@@ -1,62 +1,62 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int ft_print_hex_lower(unsigned int num)
+int	ft_print_hex_lower(unsigned int num)
 {
-    char    buffer[8];
-    char    *digits;
-    int     count;
-    int     i;
+	char	buffer[8];
+	char	*digits;
+	int		count;
+	int		i;
 
-    i = 0;
-    digits = "0123456789abcdef";
-    count = 0;
-    if (num == 0)
-    {
-        count += write (1, "0", 1);
-        return (count);
-    }
-    while (num != 0)
-    {
-        buffer[i++] = digits [num % 16];
-        num /= 16;
-    }
-    while (i > 0)
-    {
-        count += write (1, &buffer[--i], 1);
-    }
-    return (count);
+	i = 0;
+	digits = "0123456789abcdef";
+	count = 0;
+	if (num == 0)
+	{
+		count += write (1, "0", 1);
+		return (count);
+	}
+	while (num != 0)
+	{
+		buffer[i++] = digits [num % 16];
+		num /= 16;
+	}
+	while (i > 0)
+	{
+		count += write (1, &buffer[--i], 1);
+	}
+	return (count);
 }
+
+#include <stdio.h>
+#include <limits.h>
+
+int ft_print_hex_lower(unsigned int num);
 
 int main(void)
 {
-    unsigned int numbers[] = {0, 15, 16, 255, 256, 4095, 65535, 12345678};
-    int num_count = sizeof(numbers) / sizeof(numbers[0]);
-    int count_chars;
+    unsigned int tests[] = {0, 1, 10, 15, 16, 255, 1024, 123456, UINT_MAX};
+    int test_count = sizeof(tests) / sizeof(tests[0]);
+    int count;
     int i;
 
-    printf("=== Prueba de ft_print_hex_lower ===\n");
-    for (i = 0; i < num_count; i++)
+    printf("=== Pruebas ft_print_hex_lower ===\n\n");
+    
+    for (i = 0; i < test_count; i++)
     {
-        printf("Decimal: %u\n", numbers[i]);
-        printf("printf: %x\n", numbers[i]);
-        printf("ft_print_hex_lower: ");
-        count_chars = ft_print_hex_lower(numbers[i]);
-        printf("\nCaracteres impresos: %d\n\n", count_chars);
+        printf("Prueba %d: %u\n", i + 1, tests[i]);
+        printf("Esperado: %x\n", tests[i]);
+        printf("Obtenido: ");
+        count = ft_print_hex_lower(tests[i]);
+        printf("\nCaracteres impresos: %d\n\n", count);
     }
 
-    // Prueba adicional con input del usuario
-    unsigned int user_num;
-    printf("Introduce un número para convertir a hexadecimal (o 0 para salir): ");
-    scanf("%u", &user_num);
-    while (user_num != 0)
-    {
-        printf("ft_print_hex_lower: ");
-        count_chars = ft_print_hex_lower(user_num);
-        printf("\nCaracteres impresos: %d\n\n", count_chars);
-        printf("Introduce otro número (o 0 para salir): ");
-        scanf("%u", &user_num);
-    }
+    // Caso adicional: número muy grande
+    printf("Prueba adicional: 3735928559\n");
+    printf("Esperado: deadbeef\n");
+    printf("Obtenido: ");
+    count = ft_print_hex_lower(3735928559);
+    printf("\nCaracteres impresos: %d\n", count);
 
     return (0);
 }
