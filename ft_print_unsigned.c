aa154a0fb835d301f209cc1c5ebadbe3c9ce5da8
Axelgk10 <axelkonert@gmail.com>
2025-05-12 13:19:05 +0200

    Update ft_print_unsigned.c

--- ft_print_unsigned.c
+++ ft_print_unsigned.c
@@ -27,25 +27,3 @@ int	ft_print_unsigned(unsigned int num)
 	return (count);
 }
 
-int main(void)
-{
-    unsigned int numbers[] = {0, 1, 123, 4294967295, 100, 999};
-    int count = sizeof(numbers) / sizeof(numbers[0]);
-    int i;
-    int chars_printed;
-
-    i = 0;
-    while (i < count)
-    {
-        chars_printed = ft_print_unsigned(numbers[i]);
-        write(1, " (", 2);
-        // Función simple para imprimir el número de caracteres impresos
-        if (chars_printed >= 10)
-            write(1, "2", 1); // Para nuestro caso de prueba, el máximo es 4294967295 (10 dígitos)
-        else
-            write(1, &(char){chars_printed + '0'}, 1);
-        write(1, ")\n", 2);
-        i++;
-    }
-    return (0);
-}
